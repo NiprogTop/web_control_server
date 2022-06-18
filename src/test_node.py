ca18208a0c21d5eedcf15a646f0913c9b91e90d4
NiProg <kapustin20001@gmail.com>
2022-06-18 13:51:01 +0300

    test publisher

--- src/test_node.py
+++ src/test_node.py
@@ -1,1 +1,26 @@
-print("Hello NickName")
+#!/usr/bin/python
+import rospy
+import std_msgs.msg
+
+class Web:
+    def __init__(self):
+        rospy.init_node('web_server', anonymous=True)
+        self.pud = rospy.Publisher('text_line', std_msgs.msg.String, queue_size=10)
+        self.rate = rospy.Rate(10)
+
+
+    def start(ip):
+        rospy.init_node('web_server', anonymous=True)
+
+
+    def loop(self, Text):
+        while not rospy.is_shutdown():
+            self.pud.publish(Text)
+            self.rate.sleep()
+
+if __name__ == "__main__":
+    try:
+        test = Web()
+        test.loop("Hello")
+    except rospy.ROSInterruptException:
+        pass
